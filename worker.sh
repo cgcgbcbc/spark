1d9feb0ad36c903cc781a85f3685c2a5b6fc4e31
cgcgbcbc <cgcgbcbc@163.com>
2015-05-21 16:51:07 +0800

    update

--- worker.sh
+++ worker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/spark/bin/spark-class org.apache.spark.deploy.worker.Worker  spark://master:7077
+/spark/bin/spark-class org.apache.spark.deploy.worker.Worker  spark://$master:7077
 
 tail -F /spark/logs/spark*
